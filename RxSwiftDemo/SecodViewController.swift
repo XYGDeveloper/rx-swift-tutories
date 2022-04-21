371baaec236ef17a62813c3bf8d0cd26330ffe37
XYGDeveloper <15243228311@163.com>
2022-04-21 16:00:41 +0800

    - add obserble

--- RxSwiftDemo/SecodViewController.swift
+++ RxSwiftDemo/SecodViewController.swift
@@ -26,28 +26,72 @@ class SecodViewController: UIViewController {
         return Disposables.create()
     }
     
-    let json:Observable<JSON> = Observable.create { (observer) -> Disposable in
-        let task = URLSession.shared.dataTask(with: URL(string: "")!) { data, reponse, error in
-            guard error == nil else{
-                observer.onError(error!)
-                return
+//    let json:Observable<JSON> = Observable.create { (observer) -> Disposable in
+//        let task = URLSession.shared.dataTask(with: URL(string: "")!) { data, reponse, error in
+//            guard error == nil else{
+//                observer.onError(error!)
+//                return
+//            }
+//            guard let data = data,let jsonObject = try? JSONSerialization.data(withJSONObject: data, options: .fragmentsAllowed) else {
+//                observer.onError(error!)
+//                return
+//            }
+//            observer.onNext(jsonObject)
+//            observer.onCompleted()
+//        }
+//        task.resume()
+//
+//        return Disposables.create {
+//            task.cancel()
+//        }
+//    }
+    
+    func getRepo(_ repo:String) -> Single<[String:Any]> {
+        return Single<[String:Any]>.create { single in
+            let url = URL(string: "https://api.github.com/repos/\(repo)")!
+            let task = URLSession.shared.dataTask(with: url) { data, response, error in
+                if let error = error {
+                    single(.error(error))
+                }
+                guard let data = data,
+                      let json = try? JSONSerialization.jsonObject(with: data, options: .mutableLeaves),
+                      let result = json as? [String:Any] else {
+                    single(.error(error!))
+                    return
+                }
+                single(.success(result))
             }
-            guard let data = data,let jsonObject = try? JSONSerialization.data(withJSONObject: data, options: .fragmentsAllowed) else {
-                observer.onError(error!)
-                return
+            task.resume()
+            return Disposables.create {
+                task.cancel()
             }
-            observer.onNext(jsonObject)
-            observer.onCompleted()
-        }
-        task.resume()
-        
-        return Disposables.create {
-            task.cancel()
         }
     }
     
+//    func cacheLocal() -> Completable {
+//        return Completable.create { complete in
+//            /* 进行缓存操作
+//                ....
+//                ....
+//            */
+//            guard let success else{
+//                completable(.error(CacheError.failedCaching))
+//                return Disposables.create {}
+//            }
+//            complete(.completed)
+//            return Disposables.create{}
+//        }
+//    }
     
-    
+    func generalString() -> Maybe<String> {
+        return Maybe<String>.create { maybe in
+            maybe(.success("RXSWIFT"))
+            maybe(.completed)
+            let error = NSError()
+            maybe(.error(error))
+            return Disposables.create {}
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -58,17 +102,50 @@ class SecodViewController: UIViewController {
         })
         .disposed(by: disposeBag)
         //
-        json.subscribe(onNext: {
-            jsonObject in
-            print(jsonObject)
+//        json.subscribe(onNext: {
+//            jsonObject in
+//            print(jsonObject)
+//        }, onError: {
+//            error in
+//            print(error)
+//        }, onCompleted: {
+//            print("finish")
+//        })
+//        .disposed(by: disposeBag)
+        
+        
+        getRepo("XYGDeveloper/RemoteImageView_swiftSPM")
+            .subscribe(onSuccess: {
+                result in
+                print(result)
+            }, onError: {
+                error in
+                print(error)
+            })
+            .disposed(by: disposeBag)
+        
+        
+        
+        
+//        cacheLocal().subscribe(onCompleted: {
+//
+//        }, onError: {
+//            error in
+//            print(error)
+//        })
+//        .disposed(by: disposeBag)
+        
+        generalString().subscribe(onSuccess: {
+            success in
+            print("get maybe string is\(success)")
         }, onError: {
             error in
             print(error)
         }, onCompleted: {
-            print("finish")
+            print("complete")
         })
         .disposed(by: disposeBag)
-        // Do any additional setup after loading the view.
+        
     }
     
 
