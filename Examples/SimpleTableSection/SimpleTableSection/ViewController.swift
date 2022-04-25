85100ec0754ec578d3e1ae0d5e80a16051b59b9e
XYGDeveloper <15243228311@163.com>
2022-04-25 16:52:43 +0800

    - - rxTableview and rxdatasource

---
+++ Examples/SimpleTableSection/SimpleTableSection/ViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ViewController.swift
+//  SimpleTableSection
+//
+//  Created by xwtech on 2022/4/25.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import RxDataSources
+class ViewController: UIViewController,UITableViewDelegate{
+
+    @IBOutlet weak var tableview: UITableView!
+    
+    var dispose = DisposeBag()
+    
+    let dataSource = RxTableViewSectionedReloadDataSource<SectionModel<String,Double>>(
+        configureCell:  { _, tableview, indexpath, item in
+            let cell = tableview.dequeueReusableCell(withIdentifier: "cell")
+            cell?.textLabel?.text = "\(item) @ \(indexpath.row)"
+            return cell!
+        },
+        titleForHeaderInSection: { datasource, sectionIndex in
+            // title of header
+            return datasource[sectionIndex].model
+        }
+    )
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let dataSource = dataSource
+        let items = Observable.just([
+            SectionModel(model: "First Section", items: [1.0,2.0,3.0]),
+           SectionModel(model: "Second Section", items:  [1.0,2.0,3.0]),
+           SectionModel(model: "Third Section", items:  [1.0,2.0,3.0])
+        ])
+        
+        items.bind(to: tableview.rx.items(dataSource: dataSource))
+            .disposed(by: dispose)
+        
+        //点击事件
+        tableview.rx.itemSelected
+            .map{ indexPath in return (indexPath,dataSource[indexPath])}
+            .subscribe(onNext: {
+                pair in
+                print("\(pair.0)--\(pair.1)")
+            }, onError: {
+                error in
+                print(error)
+            })
+            .disposed(by: dispose)
+        
+        tableview.rx.setDelegate(self).disposed(by: dispose)
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        .none
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40.0
+    }
+}
+
+
+
+extension UITableView{
+    func hiddenEmptyCell(){
+        self.tableFooterView = UIView(frame: .zero)
+    }
+}
+
